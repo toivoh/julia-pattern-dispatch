79b995ea94fffcc8f787ff0efb20d2f421e51ff1
Toivo Henningsson <toivo.h.h@gmail.com>
2012-05-18 07:28:36 +0200

    Imported some tests from before.

--- dev/test/test_code_pmatch.jl
+++ dev/test/test_code_pmatch.jl
@@ -19,3 +19,27 @@ println()
 @pshowln code_pmatch((1, X), :args) 
 println()
 @pshowln code_pmatch({1, X}, :args) 
+
+
+function show_code_pmatch(p,vars::PVar...)
+    println()
+    println("pattern = ", p)
+
+    c = PMContext()
+    code_pmatch(c, p,:x)
+    println("vars = ", c.assigned_vars)
+#    println("code:")
+#    foreach(x->println("\t", x), c.code)
+    pprintln(expr(:block, c.code))
+end
+
+#@pvar X, Xi::Int
+X = pvar(:X)
+
+show_code_pmatch(1)
+show_code_pmatch(X, X)
+#show_code_pmatch(Xi, Xi)
+show_code_pmatch((1,X), X)
+show_code_pmatch((X,X), X)
+
+
---
+++ dev/test/test_unify_vectors.jl
@@ -0,0 +1,41 @@
+
+load("utils/req.jl")
+req("pmatch.jl")
+req("utils/utils.jl")
+
+let
+    X, Y, Z = map(pvar, (:X, :Y, :Z))
+    
+    @symshowln unify(X, {1,2})
+    @symshowln unify({X,2}, {1,2})
+    @symshowln unify({X,1}, {1,2})
+    @symshowln unify({1,X}, {1,2})
+    @symshowln unify({1,X}, {Y,2})
+    @symshowln unify({1,X}, {1,2,3})
+    @symshowln unify({1,X,Y}, {1,2,3})
+    @symshowln unify({1,X}, {1,{2,3}})
+    
+    println()
+    @symshowln unify({1,X}, {X,Y})
+    @symshowln unify({1,X,Y}, {X,Y,Z})
+    @symshowln unify({1,X,Y}, {X,Y,1})
+    @symshowln unify({1,X,Y}, {X,Y,2})
+    @symshowln unify({1,Y,X}, {X,Z,Y})
+    @symshowln unify(X, {1,X})
+
+#     println()
+#     @pvar Xi::Int, Xa::Array, Xv::Vector, Xm::Matrix
+#     @symshowln unify(Xi, {1,2})
+#     @symshowln unify(Xa, {1,2})
+#     @symshowln unify(Xv, {1,2})
+#     @symshowln unify(Xm, {1,2})
+
+#     println()
+#     @pvar Xai::Array{Int}
+#     @symshowln unify(Xai, {1,2})
+#     @symshowln unify(Xai, [1,2])
+#     @symshowln unify(Xa, [1,2,X])
+
+#     # consider: Should this work? And force X to be an Int.
+#     @symshowln unify(Xai, [1,2,X])
+end
