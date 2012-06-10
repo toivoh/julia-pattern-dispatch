94526d4a500ee25c3ccf309c4dc38191a7e14319
Toivo Henningsson <toivo.h.h@gmail.com>
2012-06-10 07:29:58 +0200

    Adapted @patmethod and wrote a simple test.

---
+++ pattern/dispatch.jl
@@ -0,0 +1,56 @@
+
+load("pattern/req.jl")
+req("pattern/recode.jl")
+
+predstr = string(:( (fdef.head == :function) || (fdef.head == :(=)) ))
+
+function split_fdef(fdef::Expr)
+    @expect (fdef.head == :function) || (fdef.head == :(=))
+    @expect length(fdef.args) == 2
+    signature, body = tuple(fdef.args...)
+    @expect is_expr(signature, :call)
+    @expect length(signature.args) >= 1
+    (signature, body)
+end
+split_fdef(f::Any) = error("split_fdef: expected function definition, got\n$f")
+
+
+# -- @patmethod ---------------------------------------------------------------
+
+type PatternMethod
+    signature::PNode
+    body
+
+    dispfun::Function
+end
+function patmethod(signature::PNode, body)
+    dispfun = create_patmethod_dispfun(signature, body)
+    PatternMethod(signature, body, dispfun)
+end
+
+function create_patmethod_dispfun(signature::PNode, body)
+    eval(code_patmethod_dispfun(signature, body))
+end
+function code_patmethod_dispfun(signature::PNode, body)
+    argsname = gensym("args")
+    vars, code = code_match(signature, argsname, :(false,nothing))
+    :( (($argsname)...)->(begin
+        ($code)
+        (true, ($body))
+    end))
+end
+
+
+macro patmethod(fdef)
+    code_patmethod(fdef)
+end
+function code_patmethod(fdef)
+    defsig, body = split_fdef(fdef)
+    @expect is_expr(defsig, :call)
+    signature_ex = expr(:tuple, defsig.args[2:end])
+
+    signature_ex = recode_patex(signature_ex)
+
+    # evaluates the signature expression inline
+    :( patmethod(($signature_ex), ($quot(body))) )
+end
---
+++ pattern/test/test_patmethod.jl
@@ -0,0 +1,19 @@
+
+load("pattern/req.jl")
+req("pattern/dispatch.jl")
+req("pattern/test/utils.jl")
+
+m1 = (@patmethod f(1) = 42)
+m2 = (@patmethod f(x) =  x)
+
+for x=0:5
+    r1 = m1.dispfun(x)
+    if x==1
+        @test r1==(true,42)
+    else
+        @test r1==(false,nothing)
+    end
+    
+    r2=m2.dispfun(x)
+    @test r2==(true,x)
+end
