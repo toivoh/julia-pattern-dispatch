06c990880c779b15b0e93e5ab6d94df56f82396e
Toivo Henningsson <toivo@control.lth.se>
2012-06-11 15:41:18 +0200

    Added some dispatch tests. Not all pass...

--- pattern/test/test_dispatch.jl
+++ pattern/test/test_dispatch.jl
@@ -6,4 +6,23 @@ req("pattern/test/utils.jl")
 @pattern f(1) = 42
 @pattern f(x) =  x
 
-@show {f(x) for x=0:4}
+@test {f(x) for x=0:4}=={0,42,2,3,4}
+
+
+@pattern f2((x,y)) = x*y
+@pattern f2(x) = nothing
+
+@test f2((2,5)) == 10
+@test f2((4,3)) == 12
+@test is(nothing, f2(1) )
+@test is(nothing, f2("hello") )
+@test is(nothing, f2((1,)) )
+@test is(nothing, f2((1,2,3)) )
+
+
+println("\n# Repeated arguments are allowed:") 
+@pattern eq(x,x) = true
+@pattern eq(x,y) = false
+
+@show eq(1,1)
+@show eq(1,2)
