b0a91716fb3e89d5ac22ed4b898994b8b77ea8e9
Toivo Henningsson <toivo@control.lth.se>
2012-08-13 16:26:51 +0200

    Some fixes for @immutable type

--- minimal/immutable.jl
+++ minimal/immutable.jl
@@ -21,17 +21,27 @@ end
     quot(ObjectIdDict())
 end
 
+record_fields!(fields::Vector{Symbol}, ex::Symbol) = (push(fields,ex); nothing)
+function record_fields!(fields::Vector{Symbol}, ex::Expr)
+    if (ex.head === :block) 
+        for arg in ex.args; record_fields!(fields, arg); end
+    elseif (ex.head === doublecolon) 
+        record_fields!(fields, ex.args[1])
+    end
+end
+record_fields!(fields::Vector{Symbol}, ex) = nothing
+
 macro immutable(ex)
     @expect is_expr(ex, :type, 2)
     typesig, defs = ex.args[1], ex.args[2]
-    typespec = is_expr(typesig, :comparison) ? typesig.args[1] : typesig
+#    typespec = is_expr(typesig, :comparison) ? typesig.args[1] : typesig
+    typespec = is_expr(typesig, :(<:)) ? typesig.args[1] : typesig
+    @show typespec
     typename = (is_expr(typespec,:curly) ? typespec.args[1] : typespec)::Symbol
 
     @expect is_expr(defs, :block)
-    fieldsf = filter(ex->isa(ex, Symbol),
-              {is_expr(ex, doublecolon) ? ex.args[1] : ex for ex in defs.args})
     fields = Symbol[]
-    for field in fieldsf; push(fields, field); end
+    record_fields!(fields, defs)
 
     defs = quote
         ($defs.args...)
