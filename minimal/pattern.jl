06348d7c44758641a1b5f3b4904d19f5e392e485
Toivo Henningsson <toivo@control.lth.se>
2012-08-13 13:04:07 +0200

    Started to sketch method sorting for PatternFunction.

--- minimal/pattern.jl
+++ minimal/pattern.jl
@@ -10,20 +10,50 @@ end
 
 function add(pf::PatternFunction, m::PatternMethod)
     push(pf.methods, m)
+    #addmethod(pf, m)
     eval( code_pattern_function(pf.fname, pf.methods) )
+end
+
+function addmethod(pf::PatternFunction, m::PatternMethod)
+    ms = pf.methods
+    n = length(ms)
+    i = n+1
+    for k = 1:n
+        if !(feas(m.sig) <= feas(ms[k].sig)) continue end
+        # equal signature ==> replace
+        if   feas(m.sig) == feas(ms[k].sig) ms[k] = m; return
+        else                                i = k; break
+        end
+    end
+    pf.methods = PatternMethod[ms[1:i-1]..., m, ms[i:n]...]
+    check_ambiguity(pf, m)
+end
+
+function check_ambiguity(pf::PatternFunction, m::PatternMethod)
+    ms = pf.methods
+    for m0 in ms
+        product = meet(feas(m0), feas(m))
+        if is_infeasible(product) continue end
+        if any([feas(mk) == feas(product) for mk in ms]) continue end
+        
+        println("Warning: New @pattern method ", pf.fname, m.sig)
+        println("         is ambiguous with   ", pf.fname, m0.sig)
+        println("         Make sure           ", pf.fname, product,
+                " is defined first.")
+    end
 end
 
 # ---- @pattern ---------------------------------------------------------------
 
 const patfun_table = Dict{Function,PatternFunction}()
 
-get_pf(fname::Symbol, ::Nothing) = PatternFunction(fname)
-function get_pf(fname::Symbol, f::Function)
-    if !has(patfun_table, f) pf_error(fname); end
+get_patfun(fname::Symbol, ::Nothing) = PatternFunction(fname)
+function get_patfun(fname::Symbol, f::Function)
+    if !has(patfun_table, f) patfun_error(fname); end
     patfun_table[f]
 end
-get_pf(fname::Symbol, f) = pf_error(fname)
-function pf_error(fname::Symbol)
+get_patfun(fname::Symbol, f) = patfun_error(fname)
+function patfun_error(fname::Symbol)
     error("\nin @pattern method definition: $fname is not a pattern function")
 end
 
@@ -38,7 +68,7 @@ function code_pattern(fdef)
         try
             f = ($esc(fname))
         end
-        pf = get_pf($quot(fname), f)
+        pf = get_patfun($quot(fname), f)
         add(pf, $method_ex)
         
         if f === nothing; patfun_table[$esc(fname)] = pf; end
